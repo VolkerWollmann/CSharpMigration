677dfe71ee1c4983949dbb9b58248c4dc155ebbb
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2022-03-25 12:25:09 +0100

    NET Framework 480 Library

---
+++ ConsoleApp480/ProgramNetFramework480.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp472
+{
+    internal class ProgramNetFramework472
+    {
+        static void Main(string[] args)
+        {
+            ;
+
+            CS73NetFramework472NameSpace.CS73NetFramework472Class cs73NetFramework472Class = new CS73NetFramework472NameSpace.CS73NetFramework472Class();
+            cs73NetFramework472Class.Method(1);
+
+            CS73Standard20NameSpace.CS73Standard20Class cs73Standard20Class = new CS73Standard20NameSpace.CS73Standard20Class();
+            cs73Standard20Class.Method(1);
+
+            //Top must be newest:
+            //CS80Standard21NameSpace.CS80Standard21Class cs80Standard21Class = new CS80Standard21NameSpace.CS80Standard21Class();
+            //cs80Standard21Class.Method(2);
+
+            //CS90Net50NameSpace.CS90Net50Class cs90Net50Class = new CS90Net50NameSpace.CS90Net50Class();
+            //cs90Net50Class.Method(3);
+        }
+    }
+}
+
